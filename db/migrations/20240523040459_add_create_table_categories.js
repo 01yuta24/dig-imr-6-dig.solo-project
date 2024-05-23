7a16ad23d4ee31706735ac02a4493853f8ae214e
Yuta Sasaki <yuta.mybusiness@gmail.com>
2024-05-23 13:19:03 +0900

    add(knexfile.js) : categoriesテーブルを追加

---
+++ db/migrations/20240523040459_add_create_table_categories.js
@@ -0,0 +1,16 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable('categories', (table) => {
+        table.increments('id').primary();
+        table.string('name');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {};
