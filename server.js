91224e4b5721f225b704803403d40d6961d8d534
Yuta Sasaki <yuta.mybusiness@gmail.com>
2024-05-23 11:36:58 +0900

    add : server.js getをテスト的に実装

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 });
