d32aa885a2fbcb2706ca119001c0e1e25fc245bd
Yuta Sasaki <yuta.mybusiness@gmail.com>
2024-05-28 09:20:37 +0900

    fix(sql) : categoriesがバグっていたので作り直し

--- db/sql/test_seed.sql
+++ db/sql/test_seed.sql
@@ -2,17 +2,17 @@ insert into categories(name) values ('vegetables');
 insert into categories(name) values ('fruits');
 insert into items(name,category_id) values ('tomato',1);
 
-insert into categories(name) values
-('野菜・果物'),
-("鮮魚"),
-('精肉'),
-('惣菜'),
-('麺・納豆・豆腐'),
-('乳製品'),
-('飲料'),
-('保存食・菓子・米'),
-('冷凍物'),
-('調味料'),
-('日用品'),
+insert into categories(id,name) values
+        (1,'野菜・果物' ),
+        (2,'鮮魚' ),
+        (3,'精肉' ),
+        (4,'惣菜' ),
+        (5,'麺・納豆・豆腐' ),
+        (6,'乳製品' ),
+        (7,'飲料' ),
+        (8,'保存食・菓子・米' ),
+        (9,'冷凍物' ),
+        (10,'調味料' ),
+        (11.'日用品')
 ;
 
